d818a8d6553709322a8ca2611245e0df409b7f43
Max Grossman <m.grossman1813@gmail.com>
2022-09-02 00:51:48 -0400

    get the indexer use sqlite query to get its history files then just go on for ever and ever

--- fixture/index_bouy_data/src/main.rs
+++ fixture/index_bouy_data/src/main.rs
@@ -6,7 +6,8 @@ use std::env;
 use std::io::Read;
 use std::collections::HashMap;
 use std::path::Path;
-use sqlite;
+use rusqlite::{Connection, NO_PARAMS};
+
 
 const MAX_BITS_MASK_3D: u64 = 0x1fffff;
 const MAX_BITS_MASK_2D: u32 = 0x0000ffff;
@@ -30,87 +31,97 @@ fn load_into_bits_2d(mut x: u32) -> u32 {
     return x
 }
 
+fn main() -> () {
+    let mut args_vec: Vec<String> = env::args().collect::<Vec<String>>();
+    let bouys_db: String = args_vec.pop().unwrap();
+    let mut connection = Connection::open(bouys_db).unwrap();
+    // get list of bouys, their timezone ids, and all the known history files
+    let mut stmt = connection.prepare(
+        "
+        SELECT bouys.id, bouys.tzid, group_concat(bouy_history.filename) 
+        FROM bouys join bouy_history on bouys.id=bouy_history.bouy_id
+        WHERE bouys.tzid != ''
+        GROUP BY bouys.id;
+        "
+    ).unwrap();
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args: HashMap<String,String> = env::args().into_iter()
-        .filter(|arg| arg.contains("="))
-        .map(|arg| {
-            let split: Vec<&str> = arg.split("=").collect();
-            (String::from(split[0]),String::from(split[1]))
-        })
-        .collect();
+    let mut rows = stmt.query(NO_PARAMS).unwrap();
 
-    let timezone_str = args.get("timezone");
-    let url = args.get("url");
-    let bouy_id = args.get("bouy"); 
+    let history_base_url = "https://www.ndbc.noaa.gov/data/historical/stdmet";
 
-    if timezone_str.is_none() || url.is_none() || bouy_id.is_none() {
-        panic!("missing required arguments timezone, url, and bouy");
-    }
-
-    // "America/California";
-    // "https://www.ndbc.noaa.gov/data/historical/stdmet/46224h2021.txt.gz";
-    let client_result = reqwest::blocking::Client::builder()
+    let client = reqwest::blocking::Client::builder()
         .gzip(true)
-        .build();
+        .build()
+        .unwrap();
 
-    match client_result {
-        Ok(client) => {
-            let bytes = client.get(url.unwrap()).send()?.bytes()?;
+    while let Some(res_row) = rows.next().unwrap() {
+        let bouy_id: String = res_row.get(0).unwrap();
+        let tzid: String = res_row.get(1).unwrap();
+        let history_files: String = res_row.get(2).unwrap();
+
+        let bouy_db = &format!("{}.sqlite", bouy_id);
+        let db: &Path = Path::new(bouy_db);
+        let db_exists = db.exists();
+
+        let mut bouy_db_connection = Connection::open(bouy_db).unwrap();
+        if !db_exists {
+            bouy_db_connection.execute_batch("
+                BEGIN;
+                CREATE TABLE timestamps (reading_time timestamp primary key,
+                                        dhp bigint,
+                                        dh  bigint,
+                                        dp  bigint,
+                                        hp  bigint,
+                                        d   bigint,
+                                        h   bigint,
+                                        p   bigint);
+
+                CREATE INDEX indx_dhp on timestamps(dhp);
+                CREATE INDEX indx_dh on timestamps(dh);
+                CREATE INDEX indx_dp on timestamps(dp);
+                CREATE INDEX indx_hp on timestamps(hp);
+                CREATE INDEX indx_d on timestamps(d);
+                CREATE INDEX indx_h on timestamps(h);
+                CREATE INDEX indx_p on timestamps(p);
+                COMMIT;
+          ").unwrap();
+        }
+
+        println!("Indexing {}", bouy_id);
+
+        for history_file in history_files.as_str().split(",").collect::<Vec<&str>>() {
+            let mut bouy_statement = String::from("BEGIN TRANSACTION;");
+            let mut indexes: HashMap<String, usize> = HashMap::new();
+            let bytes = client.get(String::from(format!("{base}/{file}", base=history_base_url, file=history_file)))
+                .send().unwrap()
+                .bytes().unwrap();
+
             let b: &[u8] = &bytes.to_vec();
             let mut gz = GzDecoder::new(b);
             let mut table = String::new();
-            gz.read_to_string(&mut table)?;
+            gz.read_to_string(&mut table).unwrap();
 
-            let mut indexes: HashMap<String, usize> = HashMap::new();
-            let db_path_str = &format!("{}.sqlite", bouy_id.unwrap());
-            let db: &Path =  Path::new(db_path_str);
-            let db_exists: bool = db.exists();
-            let connection = sqlite::open(db.to_str().unwrap()).unwrap();
+            let timezone: Tz = tzid.parse().unwrap();
+            let mut have_min = false;
+            let year_keys: [&str;2] = ["YY","YYYY"];
+            let mut year_key_idx: usize = 0;
 
-
-            connection.execute("BEGIN TRANSACTION;").unwrap();
-            if !db_exists {
-                connection.execute(
-                    "
-                    CREATE TABLE timestamps (reading_time timestamp primary key,
-                                             dhp bigint,
-                                             dh  bigint,
-                                             dp  bigint,
-                                             hp  bigint,
-                                             d   bigint,
-                                             h   bigint,
-                                             p   bigint);
-
-                    CREATE INDEX indx_dhp on timestamps(dhp);
-                    CREATE INDEX indx_dh on timestamps(dh);
-                    CREATE INDEX indx_dp on timestamps(dp);
-                    CREATE INDEX indx_hp on timestamps(hp);
-                    CREATE INDEX indx_d on timestamps(d);
-                    CREATE INDEX indx_h on timestamps(h);
-                    CREATE INDEX indx_p on timestamps(p);
-                    "
-                )
-                .unwrap();
-            }
-            //TODO: FIGURE OUT HOW CHRONO_TZ MAKES ITS TIMEZONE NAMES AND THEN FIGURE OUT IF
-            //THERE'S SOFTWARE OUT THERE THAT I USE TO RETRIEVE A NAME GIVEN A LON/LAT
-            let timezone: Tz = timezone_str.unwrap().parse().unwrap();
             for (i,line) in table.lines().enumerate() {
                 if i == 0 {
                     for (i, header) in line.split_whitespace().enumerate() {
                         indexes.insert(String::from(header.replace("#","")),i);
                     }
+                    if indexes.contains_key("YYYY") { year_key_idx += 1; }
+                    have_min = indexes.contains_key("mm");
                 } else if i > 1 {
                     let data: Vec<&str> = line.split_whitespace().collect();
                     let swell_direction = data[*indexes.get("MWD").unwrap()].parse::<f32>().unwrap();
                     if swell_direction < 0.0 || 360.00 < swell_direction {
                         continue
                     }
-                    //TODO: this will come from function param
 
                     let maybe_timezone_ymd = timezone.ymd_opt(
-                        data[*indexes.get("YY").unwrap()].parse::<i32>().unwrap(),
+                        data[*indexes.get(year_keys[year_key_idx]).unwrap()].parse::<i32>().unwrap(),
                         data[*indexes.get("MM").unwrap()].parse::<u32>().unwrap(),
                         data[*indexes.get("DD").unwrap()].parse::<u32>().unwrap()
                     );
@@ -123,7 +134,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
                     let maybe_timezone_ymd_hms = timezone_ymd.and_hms_opt(
                         data[*indexes.get("hh").unwrap()].parse::<u32>().unwrap(),
-                        data[*indexes.get("mm").unwrap()].parse::<u32>().unwrap(),
+                        if have_min { data[*indexes.get("mm").unwrap()].parse::<u32>().unwrap() } else { 0 },
                         0
                     );
 
@@ -153,20 +164,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                         load_into_bits_2d(wave_height) |
                         load_into_bits_2d(swell_period) >> 1;
 
-                    connection.execute(format!(
-                        "INSERT INTO TIMESTAMPS values ('{timestamp}',{dhp},{dp},{dh},{hp},{d},{h},{p});",
+                    bouy_statement.push_str(format!(
+                        "\nINSERT OR IGNORE INTO timestamps values ('{timestamp}',{dhp},{dp},{dh},{hp},{d},{h},{p});",
                         dhp=dhp_zid,dp=dp_zid,dh=dh_zid,
                         hp=hp_zid,d=swell_direction,
                         h=wave_height,p=swell_period,timestamp=utc_string
-                    )).unwrap()
+                    ).as_str())
                 };
             }
-            connection.execute("COMMIT").unwrap();
-        },
-        Err(err) => {
-            println!("Error: {}", err);
-        }
+            bouy_statement.push_str("\nCOMMIT;");
+            bouy_db_connection.execute_batch(bouy_statement.as_str()).unwrap();
+        } 
     }
-    Ok(())
 }
 
